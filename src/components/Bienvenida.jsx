0777f39a8fc475e67dedde00404e682b6447f203
DarienIgnacio <dari.urrutia@duocuc.cl>
2026-09-24 14:58:42 -0300

    Add files via upload

---
+++ src/components/Bienvenida.jsx
@@ -0,0 +1,12 @@
+function Bienvenida() {
+  const nombreProyecto = "Conecta Cultura";
+
+  return (
+    <section>
+      <h1>{nombreProyecto}</h1>
+      <p>Encuentra actividades culturales en tu comunidad.</p><hr></hr>
+    </section>
+  );
+}
+
+export default Bienvenida;
---
+++ src/components/PiePagina.jsx
@@ -0,0 +1,14 @@
+function PiePagina() {
+  return (
+    <footer className="py-4 bg-dark text-white">
+      <div className="container">
+            <p><strong>Conecta Cultura</strong></p>
+            <address>Contacto: <a
+            href="mailto:hola@conectacultura.cl">hola@conectacultura.cl</a></address>
+            <p><small>Proyecto académico · Desarrollo FullStack II</small></p>
+      </div>
+    </footer>
+  );
+}
+
+export default PiePagina;
